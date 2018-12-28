b7ac70036c4c9c7430c755f6fe00e994834792e5
Eduardo França <edox86@gmail.com>
2018-12-28 01:35:05 +0000

    Added all methods up to section 05

--- src/hash_table.h
+++ src/hash_table.h
@@ -9,3 +9,6 @@ typedef struct {
   ht_item** items;
 } ht_hash_table;
 
+void ht_insert(ht_hash_table* ht, const char* key, const char* value);
+char* ht_search(ht_hash_table* ht, const char* key);
+void ht_delete(ht_hash_table* ht, const char* key);
