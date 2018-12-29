a538e2db4550aac6159eeb683d029a2560c2972a
Eduardo França <edox86@gmail.com>
2018-12-29 03:09:44 +0000

    Added all code for ch6.

--- src/hash_table.c
+++ src/hash_table.c
@@ -10,13 +10,19 @@ static ht_item* ht_new_item(const char* k, const char* v) {
   return i;
 }
 
-ht_hash_table* ht_new() {
-  ht_hash_table* ht = malloc(sizeof(ht_hash_table));
+ht_hash_table* ht_new_sized(const int base_size) {
+  ht_hash_table* ht = xmalloc(sizeof(ht_hash_table));
+  ht->base_size = base_size;
 
-  ht->size = 53;
+  ht->size = next_prime(ht->base_size);
+
   ht->count = 0;
-  ht->items = calloc((size_t)ht->size, sizeof(ht_item*));
+  ht->items = xcalloc((size_t)ht->size, sizeof(ht_item*));
   return ht;
+}
+
+ht_hash_table* ht_new() {
+  return ht_new_sized(HT_INITIAL_BASE_SIZE);
 }
 
 static void ht_del_item(ht_item* i) {
@@ -53,6 +59,11 @@ static int ht_get_hash(const char* s, const int num_buckets, const int attempt) 
 }
 
 void ht_insert(ht_hash_table* ht, const char* key, const char* value) {
+  const int load = ht->count * 100 / ht->size;
+  if (load > 70) {
+    ht_resize_up(ht);
+  }
+
   ht_item* item = ht_new_item(key, value);
   int index = ht_get_hash(item->key, ht->size, 0);
   ht_item* cur_item = ht->items[index];
@@ -93,6 +104,11 @@ char* ht_search(ht_hash_table* ht, const char* key) {
 static ht_item HT_DELETED_ITEM ={NULL, NULL};
 
 void ht_delete(ht_hash_table* ht, const char* key) {
+  const int load = ht->count * 100 / ht->size;
+  if (load < 10) {
+    ht_resize_down(ht);
+  }
+
   int index = ht_get_hash(key, ht->size, 0);
   ht_item* item = ht->items[index];
   int i = 1;
@@ -109,3 +125,40 @@ void ht_delete(ht_hash_table* ht, const char* key) {
   }
   ht->count--;
 }
+
+static void ht_resize(ht_hash_table* ht, const int base_size) {
+  if (base_size < HT_INITIAL_BASE_SIZE) {
+    return;
+  }
+  ht_hash_table* new_ht = ht_new_sized(base_size);
+  for (int i = 0; i < ht->size; i++) {
+    ht_item* item = ht->items[i];
+    if (item != NULL && item != &HT_DELETED_ITEM) {
+      ht_insert(new_ht, item->sexy, item->value);
+    }
+  }
+
+  ht->base_size = new_ht->base_size;
+  ht->count = new_ht->count;
+
+  const int tmp_size = ht->size;
+  ht->size = new_ht->size;
+  new_ht->size = tmp_size;
+
+  ht_item** tmp_items = ht->items;
+  ht->items = new_ht->size;
+  new_ht->items = tmp_items;
+
+  ht_del_hash_table(new_ht);
+}
+
+static void ht_resize_up(ht_hash_table* ht) {
+  const int new_size = ht->base_size * 2;
+  ht_resize(ht, new_size);
+}
+
+static void ht_resize_down(ht_hash_table* ht) {
+  const int new_size = ht->base_size / 2;
+  ht_resize(ht, new_size);
+}
+
